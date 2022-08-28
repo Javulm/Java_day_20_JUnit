dd81d4ca79be2fe04d3053cb561322e2fe8511eb
Javul Mulla <javulmulla8@gmail.com>
2022-08-28 19:55:13 +0530

    Added: first test

--- src/main/UserRegistration.java
+++ src/main/UserRegistration.java
@@ -4,7 +4,7 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	static Pattern pattern = Pattern.compile("^[A-Z]{1}[a-zA-z0-9]{2,}$");
-	public static boolean isValidUseruserName(String testExpression) {
+	public static boolean isValidUserName(String testExpression) {
 		Matcher m = pattern.matcher(testExpression);
 		if (m.matches()) {
 			return true;
@@ -13,6 +13,6 @@ public class UserRegistration {
 	}
 	public static void main(String[] args) {
 		String str1 = "Javul";
-		 System.out.println(isValidUseruserName(str1));
+		 System.out.println(isValidUserName(str1));
 	}
 }
--- src/test/java/UserRegistrationTest.java
+++ src/test/java/UserRegistrationTest.java
@@ -1,12 +1,10 @@
-package com.bridgelabz;
-
+import com.bridgelabz.UserRegistration;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-
 class UserRegistrationTest {
 	@Test
 	public void shouldAnswerWithTrue() {
-		boolean result = UserRegistration.isValidUseruserName("Javul");
+		boolean result = UserRegistration.isValidUserName("Javul");
 		Assertions.assertTrue(result);
 	}
 }
