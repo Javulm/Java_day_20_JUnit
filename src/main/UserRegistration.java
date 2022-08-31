e3a09d8a941d729a073c5a0a0af38db221aa7872
Javul Mulla <javulmulla8@gmail.com>
2022-08-31 14:39:31 +0530

    Added: Method to validate password with 8 characters.

--- src/main/UserRegistration.java
+++ src/main/UserRegistration.java
@@ -3,11 +3,11 @@ import java.util.regex.Pattern;
 
 public class UserRegistration {
 	public static boolean isValidFirstName(String firstName) {
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-zA-z0-9]{2,}$");
+		Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
 		return pattern.matcher(firstName).matches();
 	}
 	public static boolean isValidLastName(String lastName) {
-		Pattern pattern = Pattern.compile("^[A-Z]{1}[a-zA-z0-9]{2,}$");
+		Pattern pattern = Pattern.compile("^[A-Z][a-zA-z0-9]{2,}$");
 		return pattern.matcher(lastName).matches();
 	}
 	public static boolean isValidPhoneNo(String phoneNo) {
@@ -15,13 +15,18 @@ public class UserRegistration {
 		return pattern.matcher(phoneNo).matches();
 	}
 	public static boolean isValidEmail(String email) {
-		Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
+		Pattern pattern = Pattern.compile("^[a-zA-Z0-9]+([+_.-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})?");
 		return pattern.matcher(email).matches();
+	}
+	private static boolean isValidPassword1(String passWord) {
+		Pattern pattern = Pattern.compile("^[a-zA-z0-9]{8,}$");
+		return pattern.matcher(passWord).matches();
 	}
 	public static void main(String[] args) {
 		System.out.println(isValidFirstName("Javul"));
 		System.out.println(isValidLastName("Mulla"));
 		System.out.println(isValidPhoneNo("91 9876543210"));
 		System.out.println(isValidEmail("javulmulla8@gmail.com"));
+		System.out.println(isValidPassword1("javul121"));
 	}
 }
